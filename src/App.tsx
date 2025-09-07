3653a564b5da669f2453b5d2a7790b1c4b782643
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2025-09-07 11:27:47 +0800

    all

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import "./App.css"
+
+
+function App() {
+  return (
+    <>
+      <head>
+        <title>Movie App</title>
+      </head>
+      <div className="animate__animated animate__bounce">
+        Hello, Movie App!
+      </div>
+    </>
+  );
+}
+
+export default App;
